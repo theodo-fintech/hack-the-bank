795df22691beac3cd0b8c21d401f21254176f271
MollardMichael <michaelm@theodo.fr>
2018-07-08 16:52:57 +0200

    Enable maintenance mode

--- src/main/java/com/sipios/bank/controller/HomeController.java
+++ src/main/java/com/sipios/bank/controller/HomeController.java
@@ -2,8 +2,8 @@ package com.sipios.bank.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
@@ -11,8 +11,12 @@ public class HomeController {
     String appName;
 
     @GetMapping("/")
-    public String homePage(Model model) {
-        model.addAttribute("appName", appName);
-        return "home";
+    public String homePage(
+        @RequestParam(name="debug", defaultValue="false") Boolean debug
+    ) {
+        if (debug)
+            return "home";
+
+        return "maintenance";
     }
 }
